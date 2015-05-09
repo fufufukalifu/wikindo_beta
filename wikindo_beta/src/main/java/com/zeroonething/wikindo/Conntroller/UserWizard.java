907ac23ba2200a9da63f8e17e2fda520ce5c8f13
azizz <aziz@mail.unpas.ac.id>
2015-05-09 13:16:54 +0700

    idUser dan idOrang udah generate outo

--- wikindo_beta/src/main/java/com/zeroonething/wikindo/Conntroller/UserWizard.java
+++ wikindo_beta/src/main/java/com/zeroonething/wikindo/Conntroller/UserWizard.java
@@ -10,12 +10,16 @@ package com.zeroonething.wikindo.Conntroller;
  * @author Bebek
  */
 import com.zeroonething.wikindo.Koneksi.KoneksiPostgre;
+import com.zeroonething.wikindo.Model.Article;
 import com.zeroonething.wikindo.Model.User;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
@@ -28,6 +32,7 @@ import org.primefaces.event.FlowEvent;
 public class UserWizard implements Serializable {
 
     private User user = new User();
+    private int jumlahKontributor;
 
     private boolean skip;
 
@@ -39,14 +44,51 @@ public class UserWizard implements Serializable {
         this.user = user;
     }
 
+    public int countKontributor() throws SQLException {
+        Connection con = KoneksiPostgre.getConnection();
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+        List<Article> listArticle;
+        String sql = "select count(id_user) as jumlah_kontributor from users  where jabatan = 'Kontributor'";
+        pst = con.prepareStatement(sql);
+        rs = pst.executeQuery();
+        while (rs.next()) {
+            jumlahKontributor = rs.getInt("jumlah_kontributor");
+        }
+        return jumlahKontributor;
+    }
+
+    public String getIdKOntributor() throws SQLException {
+        String idTahun;
+        int tahun = Calendar.getInstance().get(Calendar.YEAR);
+        String tahunString = String.valueOf(tahun);
+        if (new UserWizard().countKontributor() <= 9) {
+            idTahun = String.valueOf(tahunString.charAt(2)) + tahunString.charAt(3) + "0200" + new UserWizard().countKontributor();
+
+        } else if(new UserWizard().countKontributor() <= 99) {
+            idTahun = String.valueOf(tahunString.charAt(2)) + tahunString.charAt(3) + "020" + new UserWizard().countKontributor();
+
+        }else{
+             idTahun = String.valueOf(tahunString.charAt(2)) + tahunString.charAt(3) + "02" + new UserWizard().countKontributor();
+
+        }
+
+        System.out.println("idTahun+02" + new UserWizard().countKontributor());
+        String idKontributor = idTahun;
+        return idKontributor;
+
+    }
+
     public void save() {
+
         try {
+
             Connection con = KoneksiPostgre.getConnection();
 
             PreparedStatement stat = con.prepareStatement(
                     "Insert into orang values(?,?,?,?,?,?,?,?,?,?)");
 
-            stat.setString(1, user.getIdOrang());
+            stat.setString(1, new UserWizard().getIdKOntributor());
             stat.setString(2, user.getNoIdentitas());
             stat.setString(3, user.getJenisIdentitas());
             stat.setString(4, user.getEmail());
@@ -64,13 +106,12 @@ public class UserWizard implements Serializable {
             stat = con.prepareStatement(
                     "Insert into users values(?,?,?,?,?)");
 
-            stat.setString(1, user.getIdOrang());
+            stat.setString(1, new UserWizard().getIdKOntributor());
             stat.setString(2, user.getUsername());
             stat.setString(3, user.getPassword());
-            stat.setString(4, "1");
-            stat.setString(5, user.getJabatan());
+            stat.setString(4, "0");
+            stat.setString(5, "Kontributor");
             stat.executeUpdate();
-            
 
         } catch (SQLException ex) {
             System.out.println("Error in saveOrang() -->" + ex.getMessage());
